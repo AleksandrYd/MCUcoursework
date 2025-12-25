61309a768fdb2b5111cc01fec61803e220ab8885
Александр <136957086+AleksandrYd@users.noreply.github.com>
2025-12-25 23:44:03 +0700

    Delete DS18B20_Terminal/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release directory

--- DS18B20_Terminal/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DS18B20Terminal_FILE E:/code/qt_projects/sys_test/DS18B20Terminal/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/DS18B20Terminal.exe)
-set(__QT_DEPLOY_TARGET_DS18B20Terminal_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DS18B20Terminal_RUNTIME_DLLS E:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6SerialPort.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
